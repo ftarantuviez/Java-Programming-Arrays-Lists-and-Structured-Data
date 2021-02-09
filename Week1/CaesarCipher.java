fb47da58f176c56a13d09bf6bbdf655703a57fa0
Francisco Tarantuviez <tarantuviezf@gmail.com>
2021-02-09 20:42:33 -0300

    feat: CaesarCipher

--- Week1/CaesarCipher.java
+++ Week1/CaesarCipher.java
@@ -1,12 +1,63 @@
 package Week1;
+import edu.duke.*;
 
 
-/**
- * Write a description of class CaesarCipher here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class CaesarCipher {
-
+    
+    public String encrypt(String input, int key){
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String shiftAlphabet = alphabet.substring(key) + alphabet.substring(0,key);
+        StringBuilder encrypted = new StringBuilder(input);
+        
+        for(int i=0; i<input.length(); i++){
+            char character = encrypted.charAt(i);
+            int idx = alphabet.indexOf(Character.toLowerCase(character));
+            
+            if (idx != -1){
+                char newChar = shiftAlphabet.charAt(idx);
+                if(Character.isLowerCase(character)) encrypted.setCharAt(i, newChar);
+                else encrypted.setCharAt(i, Character.toUpperCase(newChar));
+            }
+        }
+        
+        return encrypted.toString();
+    }
+    
+    public void testEncrypt(){
+        int key = 15;
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        String encrypted = encrypt(message, key);
+        System.out.println("key is " + key + "\n" + encrypted);
+    }
+    
+    public String encryptTwoKeys(String input, int key1, int key2){
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        String shiftedAlphabet1 = alphabet.substring(key1) + alphabet.substring(0,key1);
+        String shiftedAlphabet2 = alphabet.substring(key2) + alphabet.substring(0,key2);
+        StringBuilder encrypted = new StringBuilder(input);
+        
+        for(int i=0; i<input.length(); i++){
+            char character = encrypted.charAt(i);
+            int idx = alphabet.indexOf(Character.toLowerCase(character));
+            
+            if (idx != -1){
+                String alphabetToUse = shiftedAlphabet1;
+                if(i % 2 != 0) alphabetToUse = shiftedAlphabet2;
+                char newChar = alphabetToUse.charAt(idx);
+                if(Character.isLowerCase(character)) encrypted.setCharAt(i, newChar);
+                else encrypted.setCharAt(i, Character.toUpperCase(newChar));
+            }
+        }
+        return encrypted.toString();
+    }
+    
+    public void testEncryptTwoKeys(){
+        int key1 = 8;
+        int key2 = 21;
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        String encrypted = encryptTwoKeys(message, key1, key2);
+        System.out.println(encrypted);
+    }
 }
